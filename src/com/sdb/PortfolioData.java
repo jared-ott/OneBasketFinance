4ddcf4798245c465191880a7fad41398561b8ac6
jared-ott <bob123jared@gmail.com>
2017-04-13 16:38:54 -0500

    More method updates

--- src/com/sdb/PortfolioData.java
+++ src/com/sdb/PortfolioData.java
@@ -16,7 +16,17 @@ public class PortfolioData {
 	 * Method that removes every person record from the database
 	 */
 	public static void removeAllPersons() {
+		ConnectionManager cm = new ConnectionManager();
+		Connection conn = cm.getConnection();
+		String query = "DELETE FROM Person";
+		PreparedStatement ps = cm.prepareStatement(conn, query);
 		
+		try {
+			ps.executeUpdate();
+		} catch (Exception e) {
+			Driver.logger.warning("Update failed: " + e.getMessage());
+		}
+		cm.closeAll(conn, ps);
 	}
 
 	/**
@@ -25,7 +35,20 @@ public class PortfolioData {
 	 * @param personCode
 	 */
 	
-	public static void removePerson(String personCode) {}
+	public static void removePerson(String personCode) {
+		ConnectionManager cm = new ConnectionManager();
+		Connection conn = cm.getConnection();
+		String query = "DELETE FROM Person WHERE code = ?";
+		PreparedStatement ps = cm.prepareStatement(conn, query);
+		
+		try {
+			ps.setString(1, personCode);
+			ps.executeUpdate();
+		} catch (Exception e) {
+			Driver.logger.warning("Update failed: " + e.getMessage());
+		}
+		cm.closeAll(conn, ps);
+	}
 	
 	/**
 	 * Method to add a person record to the database with the provided data. The
@@ -41,7 +64,12 @@ public class PortfolioData {
 	 * @param country
 	 * @param brokerType
 	 */
-	public static void addPerson(String personCode, String firstName, String lastName, String street, String city, String state, String zip, String country, String brokerType, String secBrokerId) {}
+	
+	//TODO: Double insert into address and person. Look up how to retrieve key for previous insert
+	public static void addPerson(String personCode, String firstName, String lastName, String street, 
+			String city, String state, String zip, String country, String brokerType, String secBrokerId) {
+		
+	}
 	
 	/**
 	 * Adds an email record corresponding person record corresponding to the
@@ -49,19 +77,66 @@ public class PortfolioData {
 	 * @param personCode
 	 * @param email
 	 */
-	public static void addEmail(String personCode, String email) {}
+	//TODO: Is there a join table? Finish this method
+	public static void addEmail(String personCode, String email) {
+		ConnectionManager cm = new ConnectionManager();
+		Connection conn = cm.getConnection();
+		String query = "INSERT INTO Email (address) VALUES ";
+		PreparedStatement ps = cm.prepareStatement(conn, query);
+	}
 
 	/**
 	 * Removes all asset records from the database
 	 */
-	public static void removeAllAssets() {}
+	public static void removeAllAssets() {
+		removeAllPortfolios();
+		ConnectionManager cm = new ConnectionManager();
+		Connection conn = cm.getConnection();
+		String query = "DELETE FROM AssetPortfolio";
+		PreparedStatement ps = cm.prepareStatement(conn, query);
+		boolean update = false;
+		
+		try {
+			ps.executeUpdate();
+			update = true;
+		} catch (Exception e){
+			Driver.logger.warning("Update failed: " + e.getMessage());
+		}
+		
+		if (update){
+			query = "DELETE FROM Asset";
+			ps = cm.prepareStatement(conn, query);
+			try {
+				ps.executeUpdate();
+			} catch (Exception e) {
+				Driver.logger.warning("Update failed: " + e.getMessage());
+			}
+		} else {
+			Driver.logger.warning("Update failed: AssetPortfolio was not emptied.");
+		}
+		cm.closeAll(conn, ps);
+		return;
+	}
 
 	/**
 	 * Removes the asset record from the database corresponding to the
 	 * provided <code>assetCode</code>
 	 * @param assetCode
 	 */
-	public static void removeAsset(String assetCode) {}
+	public static void removeAsset(String assetCode) {
+		ConnectionManager cm = new ConnectionManager();
+		Connection conn = cm.getConnection();
+		String query = "DELETE FROM Asset WHERE code = ?";
+		PreparedStatement ps = cm.prepareStatement(conn, query);
+		
+		try {
+			ps.setString(1, assetCode);
+			ps.executeUpdate();
+		} catch (Exception e){
+			Driver.logger.warning("Update failed: " + e.getMessage());
+		}
+		cm.closeAll(conn, ps);
+	}
 	
 	/**
 	 * Adds a deposit account asset record to the database with the
@@ -70,7 +145,24 @@ public class PortfolioData {
 	 * @param label
 	 * @param apr
 	 */
-	public static void addDepositAccount(String assetCode, String label, double apr) {}
+	//TODO: Check for good parameters
+	public static void addDepositAccount(String assetCode, String label, double apr) {
+		ConnectionManager cm = new ConnectionManager();
+		Connection conn = cm.getConnection();
+		String query = "INSERT INTO Asset (code, label, apr) VALUES ?, ?, ?";
+		PreparedStatement ps = cm.prepareStatement(conn, query);
+		
+		//TODO: Check all is right
+		try {
+			ps.setString(1, assetCode);
+			ps.setString(2, label);
+			ps.setDouble(3, apr);
+			ps.executeUpdate();
+		} catch (Exception e) {
+			Driver.logger.warning("Update failed: " + e.getMessage());
+		}
+		cm.closeAll(conn, ps);
+	}
 	
 	/**
 	 * Adds a private investment asset record to the database with the
@@ -83,8 +175,28 @@ public class PortfolioData {
 	 * @param baseOmega
 	 * @param totalValue
 	 */
+	//TODO: Check for good parameters
 	public static void addPrivateInvestment(String assetCode, String label, Double quarterlyDividend, 
-			Double baseRateOfReturn, Double baseOmega, Double totalValue) {}
+			Double baseRateOfReturn, Double baseOmega, Double totalValue) {
+		ConnectionManager cm = new ConnectionManager();
+		Connection conn = cm.getConnection();
+		String query = "INSERT INTO Asset (code, label, quarterlyDividend, baseRateOfReturn, risk, value) VALUES ?, ?, ?, ?, ?, ?, ?";
+		PreparedStatement ps = cm.prepareStatement(conn, query);
+		
+		//TODO: Check all is right
+		try {
+			ps.setString(1, assetCode);
+			ps.setString(2, label);
+			ps.setDouble(3, quarterlyDividend);
+			ps.setDouble(4, baseRateOfReturn);
+			ps.setDouble(5, baseOmega);
+			ps.setDouble(6, totalValue);
+			ps.executeUpdate();
+		} catch (Exception e) {
+			Driver.logger.warning("Update failed: " + e.getMessage());
+		}
+		cm.closeAll(conn, ps);
+	}
 	
 	/**
 	 * Adds a stock asset record to the database with the
